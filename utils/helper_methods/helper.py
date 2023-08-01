2d686eec0208e8e7f6c434a3dca6f3606ac2a77f
serboctor <sara.boctor@weave.works>
2023-08-01 23:30:49 +0300

    Initial tests previously done by Tagreed on a personal repo to run automation tests against a clusters-config cluster

---
+++ utils/helper_methods/helper.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+def install_policies():
+    exit_code = subprocess.run(f"kubectl apply -f ./utils/data/policies.yaml")
+    assert exit_code == 0, "Failed to install policies to the cluster"
+
+
+def install_violated_app():
+    exit_code = subprocess.run(f"kubectl apply -f ./utils/data/violated_app.yaml")
+    assert exit_code == 0, "Failed to install violated App to the cluster"
