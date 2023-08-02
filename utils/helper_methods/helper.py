4680faba13f36398d02d37fc1e6e55a35fcdd68d
taghreed86 <tagreed_23@live.com>
2023-08-03 01:03:26 +0300

    update install policies and violated app methods

--- utils/helper_methods/helper.py
+++ utils/helper_methods/helper.py
@@ -1,11 +1,13 @@
-import subprocess
+from subprocess import run
 
 
 def install_policies():
-    exit_code = subprocess.run(f"kubectl apply -f ./utils/data/policies.yaml")
+    process = run(f"kubectl apply -f /utils/data/policies.yaml")
+    exit_code = process.returncode
     assert exit_code == 0, "Failed to install policies to the cluster"
 
 
 def install_violated_app():
-    exit_code = subprocess.run(f"kubectl apply -f ./utils/data/violated_app.yaml")
+    process = run(f"kubectl apply -f /utils/data/violated_app.yaml")
+    exit_code = process.returncode
     assert exit_code == 0, "Failed to install violated App to the cluster"
