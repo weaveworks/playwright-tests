257c6a03cc44b09d45df60b8eb6f096104cd36f5
taghreed86 <tagreed_23@live.com>
2023-08-07 22:02:27 +0300

    delete helper methods

--- utils/helper_methods/helper.py
+++
@@ -1,7 +0,0 @@
-# from subprocess import run
-#
-#
-# def install_violated_app():
-#     process = run(f"kubectl apply -f ../utils/data/violated_app.yaml")
-#     exit_code = process.returncode
-#     assert exit_code == 0, "Failed to install violated App to the cluster"
