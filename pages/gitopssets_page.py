8f9787e7ff15d9aed57cc085b31a7ed06219734e
taghreed86 <tagreed_23@live.com>
2023-12-15 05:38:26 +0200

    add gitopssets files and page

---
+++ pages/gitopssets_page.py
@@ -0,0 +1,30 @@
+class GitopsSets:
+    def __init__(self, page):
+        self.page = page
+
+    def open_gitopssets_page(self):
+        self.page.get_by_role("link", name="GitOpsSets").click()
+
+    def open_gitopssets_details_page(self):
+        self.page.get_by_role("link", name="gitopsset-configmaps").click()
+
+    def open_first_configmap_yaml(self):
+        self.page.get_by_text("dev-info-configmap").click()
+
+    def open_second_configmap_yaml(self):
+        self.page.get_by_text("staging-info-configmap").click()
+
+    def open_third_configmap_yaml(self):
+        self.page.get_by_text("production-info-configmap").click()
+
+    def open_application_events_tab(self):
+        self.page.get_by_role("tab", name="Events").click()
+
+    def open_application_graph_tab(self):
+        self.page.get_by_role("tab", name="Graph").click()
+
+    def open_application_yaml_tab(self):
+        self.page.get_by_role("tab", name="Yaml").click()
+
+    def back_to_gitopssets_list(self):
+        self.page.get_by_test_id("link-GitOpsSet").click()
--- test_weave_gitops_enterprise/test_applications.py
+++ test_weave_gitops_enterprise/test_applications.py
@@ -1,30 +1,8 @@
 import os
 
 from playwright.sync_api import Playwright, sync_playwright, expect
-from pages.policies_page import Policies
 from pages.application_page import Applications
 import pytest
-
-
-@pytest.mark.usefixtures("login")
-class TestPolicies:
-
-    @pytest.fixture(autouse=True)
-    def _obj(self, login):
-        self.page = login
-        self.policies_page = Policies(self.page)
-        self.URL = os.getenv("URL")
-
-    def test_open_policies_page(self):
-        self.policies_page.open_policies_page()
-        expect(self.page).to_have_url(f"{self.URL}/policies/list")
-
-    def test_open_policy_details_page(self):
-        self.policies_page.open_policy_details_page()
-        expect(self.page).to_have_url(f"{self.URL}/policy_details/"
-                                      f"details?clusterName=management"
-                                      f"&id=weave.policies.containers-minimum-replica-count"
-                                      f"&name=Containers%20Minimum%20Replica%20Count")
 
 
 @pytest.mark.usefixtures("login")
